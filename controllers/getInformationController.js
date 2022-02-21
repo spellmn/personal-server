e21046f5ac9bf5f399a6f83be6c32e2d1412a787
Andrei Suciu <andreisuciu2005@yahoo.com>
2022-02-21 11:59:03 +0200

    Added edit hardrock endpoint

--- controllers/getInformationController.js
+++ controllers/getInformationController.js
@@ -1,77 +1,85 @@
-const Dealership = require('../models/dealerships')
-const Tourcard = require('../models/tourcard')
-const Hardrock = require('../models/hardrock')
-const Slush = require('../models/slush')
-const { ObjectId } = require('mongodb')
+const Dealership = require('../models/dealerships');
+const Tourcard = require('../models/tourcard');
+const Hardrock = require('../models/hardrock');
+const Slush = require('../models/slush');
+const { ObjectId } = require('mongodb');
 
 const getDealerships = async (req, res, next) => {
 	if (typeof req.query.id != 'undefined') {
 		let dealership = await Dealership.find({ _id: req.query.id }).sort({
 			name: 1,
-		})
-		res.send(dealership)
+		});
+		res.send(dealership);
 	} else {
-		let dealership = await Dealership.find({}).sort({ name: 1 })
-		res.send(dealership)
+		let dealership = await Dealership.find({}).sort({ name: 1 });
+		res.send(dealership);
 	}
 	//    let dealership = await Dealership.find().sort( { "name": 1 } );
 	//    res.send(dealership);
-}
+};
 
 const deleteDealership = async (req, res, next) => {
 	if (typeof req.query.id != 'undefined') {
-		let dealership = await Dealership.find({ _id: req.query.id })
-			.sort({ name: 1 })
-			.deleteOne()
+		let dealership = await Dealership.find({ _id: req.query.id }).sort({ name: 1 }).deleteOne();
 		if (dealership.ok) {
-			res.send({ status: 200, success: true })
+			res.send({ status: 200, success: true });
 		}
-		res.send({ status: 400, success: false })
+		res.send({ status: 400, success: false });
 	}
-}
+};
 
 const getTourcard = async (req, res, next) => {
 	let tourcard = await Tourcard.find({
 		year: req.query.year,
 		association: req.query.association,
-	}).sort({ name: 1 })
-	res.send(tourcard)
-}
+	}).sort({ name: 1 });
+	res.send(tourcard);
+};
 
 const getHardrock = async (req, res, next) => {
-	let hardrock = await Hardrock.find({}).sort({ name: 1 })
-	res.send(hardrock)
-}
+	let hardrock = await Hardrock.find({}).sort({ name: 1 });
+	res.send(hardrock);
+};
+
+const putHardrock = async (req, res) => {
+	try {
+		let foundHardrock = await Hardrock.findOneAndUpdate({ _id: ObjectId(req.params.id) }, req.body, { returnDocument: 'after', returnOriginal: false });
+		foundHardrock
+			? res.send({
+					status: 'OK',
+					data: foundHardrock,
+			  })
+			: res.send({ status: 'NOT FOUND' });
+	} catch (err) {
+		res.status(500).send({ status: 'error', error: err });
+	}
+};
 
 const getSlush = async (req, res, next) => {
-	let slush = await Slush.find({}).sort({ name: 1 })
-	res.send(slush)
-}
+	let slush = await Slush.find({}).sort({ name: 1 });
+	res.send(slush);
+};
 
 const postTourcard = async (req, res) => {
 	if (req.body.name) {
-		let newTourcard = await Tourcard.create(req.body)
-		res.send({ status: 200, success: true, tourcard: newTourcard })
-	} else res.send({ status: 400, success: false })
-}
+		let newTourcard = await Tourcard.create(req.body);
+		res.send({ status: 200, success: true, tourcard: newTourcard });
+	} else res.send({ status: 400, success: false });
+};
 
 const putTourcard = async (req, res) => {
 	try {
-		let foundTourcard = await Tourcard.findOneAndUpdate(
-			{ _id: ObjectId(req.params.id) },
-			req.body,
-			{ returnDocument: 'after', returnOriginal: false }
-		)
+		let foundTourcard = await Tourcard.findOneAndUpdate({ _id: ObjectId(req.params.id) }, req.body, { returnDocument: 'after', returnOriginal: false });
 		foundTourcard
 			? res.send({
 					status: 'OK',
 					data: foundTourcard,
 			  })
-			: res.send({ status: 'NOT FOUND' })
+			: res.send({ status: 'NOT FOUND' });
 	} catch (err) {
-		res.status(500).send({ status: 'error', error: err })
+		res.status(500).send({ status: 'error', error: err });
 	}
-}
+};
 
 module.exports = {
 	deleteDealership,
@@ -81,4 +89,5 @@ module.exports = {
 	getSlush,
 	postTourcard,
 	putTourcard,
-}
+	putHardrock,
+};
--- index.js
+++ index.js
@@ -1,62 +1,57 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const { connectionString } = require('./config/db.js')
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const { connectionString } = require('./config/db.js');
 
-var getInformationController = require('./controllers/getInformationController')
-var setInformationController = require('./controllers/setInformationController')
-var informationController = require('./controllers/information')
+var getInformationController = require('./controllers/getInformationController');
+var setInformationController = require('./controllers/setInformationController');
+var informationController = require('./controllers/information');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.json({ limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }));
 app.use(
 	bodyParser.urlencoded({
 		extended: true,
 		limit: '50mb',
 	})
-)
+);
 
 app.use(function (req, res, next) {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Credentials', true)
-	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-	res.setHeader(
-		'Access-Control-Allow-Headers',
-		'Authorization,Origin,X-Requested-With,Content-Type'
-	)
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.setHeader('Access-Control-Allow-Headers', 'Authorization,Origin,X-Requested-With,Content-Type');
 	if (req.method === 'OPTIONS') {
-		return res.sendStatus(200)
+		return res.sendStatus(200);
 	}
-	next()
-})
+	next();
+});
 
 //mongoose.connect('mongodb+srv://roUser:roUserPassword@cluster0.iodve.mongodb.net/default').then(() => {
 mongoose
 	.set('useFindAndModify', false)
-	.connect(
-		'mongodb+srv://rwUser:rwUserPassword@cluster0.iwvnu.mongodb.net/default',
-		{ useNewUrlParser: true, useUnifiedTopology: true }
-	)
+	.connect('mongodb+srv://rwUser:rwUserPassword@cluster0.iwvnu.mongodb.net/default', { useNewUrlParser: true, useUnifiedTopology: true })
 	.then(() => {
-		console.log('database connected')
+		console.log('database connected');
 	})
 	.catch((err) => {
-		console.log(err)
-	})
+		console.log(err);
+	});
 
-app.get('/getDealerships', getInformationController.getDealerships)
-app.get('/deleteDealership', getInformationController.deleteDealership)
-app.post('/setDealerships', setInformationController.setDealerships)
-app.post('/editDealership', setInformationController.editDealership)
-app.get('/getTourcard', getInformationController.getTourcard)
-app.get('/getHardrock', getInformationController.getHardrock)
-app.get('/getSlush', getInformationController.getSlush)
+app.get('/getDealerships', getInformationController.getDealerships);
+app.get('/deleteDealership', getInformationController.deleteDealership);
+app.post('/setDealerships', setInformationController.setDealerships);
+app.post('/editDealership', setInformationController.editDealership);
+app.get('/getTourcard', getInformationController.getTourcard);
+app.get('/getHardrock', getInformationController.getHardrock);
+app.get('/getSlush', getInformationController.getSlush);
 
-app.post('/postTourcard', getInformationController.postTourcard)
-app.put('/putTourcard/:id', getInformationController.putTourcard)
+app.post('/postTourcard', getInformationController.postTourcard);
+app.put('/putTourcard/:id', getInformationController.putTourcard);
+app.put('/putHardrock/:id', getInformationController.putHardrock);
 
 var server = app.listen(process.env.PORT || 8001, function () {
-	var port = server.address().port
-	console.log('Express is working on port ' + port)
-})
+	var port = server.address().port;
+	console.log('Express is working on port ' + port);
+});
